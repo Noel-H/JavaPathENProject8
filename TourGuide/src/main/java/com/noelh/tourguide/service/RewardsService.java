4faaaaceed7bf758d8715611164dc628dfd2b8e5
Noel <noel.huberto@gmail.com>
2022-07-14 21:22:25 +0200

    Correct indentation added on Service

--- TourGuide/src/main/java/com/noelh/tourguide/service/RewardsService.java
+++ TourGuide/src/main/java/com/noelh/tourguide/service/RewardsService.java
@@ -22,25 +22,25 @@ public class RewardsService {
 	@Autowired
 	private RewardCentral rewardsCentral;
 
-    private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
+	private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
 
 	// proximity in miles
-    private int defaultProximityBuffer = 10;
+	private int defaultProximityBuffer = 10;
 	private int proximityBuffer = defaultProximityBuffer;
 	private int attractionProximityRange = 200;
-	
+
 	public void setProximityBuffer(int proximityBuffer) {
 		this.proximityBuffer = proximityBuffer;
 	}
-	
+
 	public void setDefaultProximityBuffer() {
 		proximityBuffer = defaultProximityBuffer;
 	}
-	
+
 	public void calculateRewards(User user) {
 		List<VisitedLocation> userLocations = user.getVisitedLocations();
 		List<Attraction> attractions = gpsUtil.getAttractions();
-		
+
 		for(VisitedLocation visitedLocation : userLocations) {
 			for(Attraction attraction : attractions) {
 				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
@@ -51,31 +51,31 @@ public class RewardsService {
 			}
 		}
 	}
-	
+
 	public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
 		return getDistance(attraction, location) > attractionProximityRange ? false : true;
 	}
-	
+
 	private boolean nearAttraction(VisitedLocation visitedLocation, Attraction attraction) {
 		return getDistance(attraction, visitedLocation.location) > proximityBuffer ? false : true;
 	}
-	
+
 	public int getRewardPoints(Attraction attraction, User user) {
 		return rewardsCentral.getAttractionRewardPoints(attraction.attractionId, user.getUserId());
 	}
-	
+
 	public double getDistance(Location loc1, Location loc2) {
-        double lat1 = Math.toRadians(loc1.latitude);
-        double lon1 = Math.toRadians(loc1.longitude);
-        double lat2 = Math.toRadians(loc2.latitude);
-        double lon2 = Math.toRadians(loc2.longitude);
+		double lat1 = Math.toRadians(loc1.latitude);
+		double lon1 = Math.toRadians(loc1.longitude);
+		double lat2 = Math.toRadians(loc2.latitude);
+		double lon2 = Math.toRadians(loc2.longitude);
 
-        double angle = Math.acos(Math.sin(lat1) * Math.sin(lat2)
-                               + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+		double angle = Math.acos(Math.sin(lat1) * Math.sin(lat2)
+				+ Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
 
-        double nauticalMiles = 60 * Math.toDegrees(angle);
-        double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
-        return statuteMiles;
+		double nauticalMiles = 60 * Math.toDegrees(angle);
+		double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
+		return statuteMiles;
 	}
 
 }
--- TourGuide/src/main/java/com/noelh/tourguide/service/TourGuideService.java
+++ TourGuide/src/main/java/com/noelh/tourguide/service/TourGuideService.java
@@ -43,29 +43,29 @@ public class TourGuideService {
 	@Autowired
 	private TripPricer tripPricer;
 
-		public List<User> getAllUsers() {
+	public List<User> getAllUsers() {
 		return internalUserApi.getInternalUserMap().values().stream().collect(Collectors.toList());
 	}
 
-		public User getUser(String userName) {
+	public User getUser(String userName) {
 		return internalUserApi.getInternalUserMap(userName);
 	}
 
-		public VisitedLocation getUserLocation(User user) {
+	public VisitedLocation getUserLocation(User user) {
 		VisitedLocation visitedLocation = (user.getVisitedLocations().size() > 0) ?
 				user.getLastVisitedLocation() :
 				trackUserLocation(user);
 		return visitedLocation;
 	}
 
-		public VisitedLocation trackUserLocation(User user) {
+	public VisitedLocation trackUserLocation(User user) {
 		VisitedLocation visitedLocation = gpsUtil.getUserLocation(user.getUserId());
 		user.addToVisitedLocations(visitedLocation);
 		rewardsService.calculateRewards(user);
 		return visitedLocation;
 	}
 
-		public List<ClosestAttractionDTO> getClosestAttractions(User user) throws NullPointerException{
+	public List<ClosestAttractionDTO> getClosestAttractions(User user) throws NullPointerException{
 		return gpsUtil.getAttractions().stream()
 				.map(attraction -> new ClosestAttractionDTO(
 						attraction.attractionName,
@@ -79,7 +79,7 @@ public class TourGuideService {
 				.collect(Collectors.toList());
 	}
 
-		public List<UserReward> getUserRewards(User user) {
+	public List<UserReward> getUserRewards(User user) {
 		return user.getUserRewards();
 	}
 
@@ -92,7 +92,7 @@ public class TourGuideService {
 				.collect(Collectors.toList());
 	}
 
-		public List<Provider> getTripDeals(User user) {
+	public List<Provider> getTripDeals(User user) {
 		int cumulatativeRewardPoints = user.getUserRewards().stream().mapToInt(i -> i.getRewardPoints()).sum();
 		List<Provider> providers = tripPricer.getPrice(internalUserApi.getTripPricerApiKey(), user.getUserId(), user.getUserPreferences().getNumberOfAdults(),
 				user.getUserPreferences().getNumberOfChildren(), user.getUserPreferences().getTripDuration(), cumulatativeRewardPoints);
@@ -100,7 +100,7 @@ public class TourGuideService {
 		return providers;
 	}
 
-		public void addShutDownHook() {
+	public void addShutDownHook() {
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
 				tracker.stopTracking();
@@ -109,7 +109,7 @@ public class TourGuideService {
 	}
 
 	public void addUser(User user){
-			internalUserApi.addUser(user);
+		internalUserApi.addUser(user);
 	}
 
 }
